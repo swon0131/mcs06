a219c9699e310d00ed0a5b299e5553c58b581fea
jkho0044 <jkho0044@gmail.com>
2024-09-24 18:18:29 +0800

    Removed MonoBehaviour

--- Assets/IATK/Scripts/Controller/PointInformation.cs
+++ Assets/IATK/Scripts/Controller/PointInformation.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 
 
-public class PointInformation : MonoBehaviour
+public class PointInformation
 {
     // Public fields or properties to hold additional information
     public int pointId;
